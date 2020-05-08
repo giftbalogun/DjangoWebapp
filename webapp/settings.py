210521c4c38891e5032e0a36becb3a7b35ecba6a
giftbalogun <blgnbalogun53@gmail.com>
2020-05-08 11:39:26 +0100

    Heroku Update

--- webapp/settings.py
+++ webapp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'ipj$kqrkcip(ucys^8v$ysir56n1)tem5(0tp0enx#f$(jw@=h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['giftbalogun.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
