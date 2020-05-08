3762e8ae9fe494adffa6b259bebccbaa6dd674ed
giftbalogun <blgnbalogun53@gmail.com>
2020-05-08 11:19:08 +0100

    fix

--- webapp/settings.py
+++ webapp/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
@@ -155,3 +155,7 @@ EMAIT_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'blgnbalogun53@gmail.com'
 EMAIL_HOST_PASSWORD = 'password040'
+
+
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
